724ff89811247505b986a70a099b5a0ae29ea5b5
Mike Marmer <s1137623@monmouth.edu>
2017-10-06 21:10:13 -0400

    changes to createacclogin

--- createacclogin.php
+++ createacclogin.php
@@ -15,20 +15,13 @@ if ($conn){
 }
 
 if (isset($_POST['createaccount'])){
-echo "<h3>Button Test</h3>";
-$username = $_POST('username');
-$firstname = $_POST('firstname');
-$lastname = $_POST('lastname');
-$email = $_POST('email');
-$password = $_POST('password');
-
 $sql = "INSERT INTO CreateAccount (username, firstname, lastname, email, password)
-VALUES ('$username', '$firstname', '$lastname', '$email', '$password')";
-if ($conn->query($sql) == TRUE) {
-  echo "<h3>Data inserted succesfully</h3>";
+VALUES ('".$_POST["username"]."', '".$_POST["firstname"]."', '".$_POST["lastname"]."', '".$_POST["email"]."', '".$_POST["password"]."')";
+$query = mysqli_query($conn,$sql);
+if ($query == TRUE) {
+  echo "<h3>Account Created</h3>";
 } else {
-  echo "<h3>Data not inserted succesfully</h3>";
+  echo "<h3>Account Creation Failed</h3>";
 }
-$conn->close();
 }
  ?>
